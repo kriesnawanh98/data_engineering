8481d3dd319ef373d0ab150a868c21cf864597c1
handy-bithealth <handy.sugianto@bithealth.co.id>
2022-11-13 23:53:59 +0700

    update_async

--- asynchronous_python/api_req_async_v2.py
+++ asynchronous_python/api_req_async_v2.py
@@ -67,7 +67,9 @@ results = []
 def get_tasks(session):
     tasks = []
     for symbol in symbols:
-        tasks.append(session.get(url.format(symbol, api_key), ssl=False))
+        tasks.append(
+            asyncio.create_task(
+                session.get(url.format(symbol, api_key), ssl=False)))
     print(tasks)
     return tasks
 
